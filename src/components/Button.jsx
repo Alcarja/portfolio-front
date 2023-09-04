e6960a005e717db1aed1f92cb1e1f9ccdb815e2e
Alcaraz <jaime.mediano@gmail.com>
2023-09-04 13:46:23 +0200

    Fixed the button link to the mail

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Button = () => {
+  const recipientEmail = "jaime.alcarazc@gmail.com";
   return (
     <>
-      <button className="button">Contact Me</button>
+      <button className="button">
+        <Link to={`mailto:${recipientEmail}`}>Contact Me</Link>
+      </button>
     </>
   );
 };
