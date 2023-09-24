c46e131922741a9eefa8e65827ec22d54552891c
Alcaraz <jaime.mediano@gmail.com>
2023-09-24 10:56:06 +0200

    Fixed the mobile version

--- src/Pages/ProjectDetailsPage.jsx
+++ src/Pages/ProjectDetailsPage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { HiMiniCodeBracket } from "react-icons/hi2";
 import { API_URL } from "../config/config.index";
 
@@ -33,29 +33,32 @@ const ProjectDetailsPage = () => {
 
   return (
     <>
-      <div className="project-title">
+      <div className="project-title-container">
         <h2>{project.title}</h2>
-        <div className="flex-container-3">
-          <HiMiniCodeBracket size={43} color={"black"} />
+        <div className="flex-container-title">
+          <HiMiniCodeBracket size={37} color={"black"} />
           <p>{project.type}</p>
         </div>
       </div>
 
-      <div className="project-description">
-        <div className="project-description-left">
-          <p>{project.description}</p>
-          <h5>Link to the app: {project.link}</h5>
-          <h5>Technologies used:</h5>
-          <div className="technologies">
-            {project.technologies.map((technology, index) => (
-              <p key={index}>{technology}</p>
-            ))}
+      <Link to={project.link} target="blank">
+        <div className="project-container">
+          <div className="project-info">
+            <div className="project-info-left">
+              <p>{project.description}</p>
+              {/* <h5>Link to the app: {project.link}</h5> */}
+              <div className="project-stack">
+                {project.technologies.map((technology, index) => (
+                  <img key={index} src={technology} alt={`Tech ${index + 1}`} />
+                ))}
+              </div>
+            </div>
+            <div className="project-info-right">
+              <img src={project.image} />
+            </div>
           </div>
         </div>
-        <div className="project-description-right">
-          <img src={project.image} className="image" />
-        </div>
-      </div>
+      </Link>
     </>
   );
 };
